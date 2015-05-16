a3d0b1b82fc5307bf5fcc152b7cab1d8d55f0405
Chase Higgins <chasehx@gmail.com>
2015-05-15 19:48:35 -0600

    mess with angular modules

--- config/routes.js
+++ config/routes.js
@@ -5,8 +5,9 @@ app.get('/api/', function(req, res) {
 	
 });
 
-// route to handle all angular requests
-app.get('*', function(req, res) {
+// routes to handle angular requests
+app.get('/', function(req, res) {
+	// index file and it's included angular app will be served
 	res.sendfile('./public/index.html'); // load our public/index.html file
 });
 
